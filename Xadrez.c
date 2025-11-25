9f5f2abeada65296d805bc91bb0cb737f79b22a8
renatosoa12-code <renatosoa12@gmail.com>
2025-11-24 21:19:15 -0300

    Update Xadrez.c

--- Xadrez.c
+++ Xadrez.c
@@ -1,44 +1,55 @@
 #define _CRT_SECURE_NO_WARNINGS
 # include <stdio.h>
 
+void torre(int t) {
+	if (t == 0) { return; }
+	printf("Direita\n");
+	torre(t - 1);
+}
+
+void rainha(int r) {
+	if (r == 0) { return; }
+	printf("Esquerda\n");
+	rainha(r - 1);
+}
+
+void bispo(int b) {
+	if (b == 0) { return; }
+	for (int v = 0; v < 5; v++) {
+		printf("Cima\n");
+		for (int h = 0; h < 1; h++)
+			printf("Direita\n");
+	}
+	bispo(b - 1);
+}
+
 int main()
 {
 
 	// VARIAVEIS
 
 
-
 	// TORRE 5 CASAS PARA DIREITA
 	printf("\n\nTORRE\n");
-	for (int i = 0; i < 5; i++)
-	{
-		printf("Direita\n");
-	}
+	torre(5);
 
 	// BISPO 5 DIAGONAL CIMA DIREITA
 	printf("\n\nBISPO\n");
-	for (int i = 0; i < 5; i++)
-	{
-		printf("Cima\nDireita\n");
-	}
-
+	bispo(1);
 	// RAINHA 8 ESQUERDA
 	printf("\n\nRAINHA\n");
-	for (int i = 0; i < 8; i++)
-	{
-		printf("Esquerda\n");
-	}
+	rainha(8);
 
-	// CAVALO BAIXO, BAIXO, ESQUERDA
+	// CAVALO CIMA CIMA DIREITA
 
 	printf("\n\nCAVALO\n");
 	for (int i = 0; i < 1; i++)
 	{
 		for (int j = 0; j < 2; j++)
 		{
-			printf("Baixo\n");
+			printf("Cima\n");
 		}
-		printf("Esquerda\n");
+		printf("Direita\n");
 	}
 
 	return 0;
